8deeedf807d3fbad6b9fea1f0344468f67e77b00
Zdendys79 <Zdendys79@gmail.com>
2025-08-03 11:23:28 +0200

    Odstranění zbytečného 20s čekání před navigací na Facebook
    PROBLÉM:
    - openFB() metoda čekala 20 sekund před navigací na Facebook
    - Způsobovalo prodlevu 12-20s při každém UI příkazu
    - Zbytečné zpomalení automatických operací
    
    ŘEŠENÍ:
    - Odstraněno Wait.toSeconds(20) z openFB() metody
    - Zachováno pouze 3s čekání po navigaci
    - UI příkazy budou rychlejší o ~15 sekund
    
    DOPAD:
    - Rychlejší zpracování UI příkazů
    - Zachována funkcionalita bez negativních efektů

--- ivy/libs/iv_fb.class.js
+++ ivy/libs/iv_fb.class.js
@@ -393,9 +393,6 @@ export class FBBot {
     try {
       await this.bringToFront();
       
-      // Lidská pauza před navigací na Facebook - prodlouženo pro ruční zásah
-      await Wait.toSeconds(20);
-      
       await this.navigateToPage('https://www.facebook.com/', { waitUntil: 'domcontentloaded' });
       await Wait.toSeconds(3);
       return true;
